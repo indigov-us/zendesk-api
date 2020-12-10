cae2148c241ab10ef355ec253164147c54b61344
Ethan Jon <ethanczahor@gmail.com>
2020-12-10 15:26:55 -0800

    added retrying to jobCompletion

--- src/job-completion.ts
+++ src/job-completion.ts
@@ -1,6 +1,7 @@
 import { RequestInit } from 'node-fetch'
 
 import { FetchMethod } from './client'
+import retry from './retry'
 import Zendesk from './zendesk'
 
 const statusUpdateInterval = 3000
@@ -9,18 +10,29 @@ export default ({
   api,
   onJobStatus,
   onProgress,
+  retryRateLimitErrors,
 }: {
   api: FetchMethod
   onJobStatus?: (jobStatus: Zendesk.JobStatus) => void
   onProgress?: (progress: number) => void
+  retryRateLimitErrors?: boolean
 }) => async (path: string, init?: RequestInit) => {
-  let jobRes = await api<Zendesk.SingleResults.JobStatus>(path, init)
+  // allow for retrying rate limits indefinitely.
+  // maxNumAttempts = 1 is the same thing as not retrying at all
+  const retryArgs: Parameters<typeof retry>[1] = {
+    maxNumAttempts: retryRateLimitErrors ? undefined : 1,
+  }
+
+  let jobRes = await retry<Zendesk.SingleResults.JobStatus>(api(path, init), retryArgs)
   let initialProgress = 0
 
   while (['queued', 'working'].includes(jobRes.body.job_status.status)) {
     await new Promise((resolve) => setTimeout(resolve, statusUpdateInterval))
 
-    jobRes = await api<Zendesk.SingleResults.JobStatus>(`/job_statuses/${jobRes.body.job_status.id}.json`)
+    jobRes = await retry<Zendesk.SingleResults.JobStatus>(
+      api(`/job_statuses/${jobRes.body.job_status.id}.json`),
+      retryArgs
+    )
 
     if (onJobStatus) onJobStatus(jobRes.body.job_status)
 
